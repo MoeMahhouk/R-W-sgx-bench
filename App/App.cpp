9c7824c42c46fe6c36f13aa5cc482c45c9b13c08
Moe <mahhouk@ibr.cs.tu-bs.de>
2019-10-08 18:37:16 +0200

    done, needs testing

---
+++ App/App.cpp
@@ -0,0 +1,263 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <time.h>
+
+# include <unistd.h>
+# include <pwd.h>
+# define MAX_PATH FILENAME_MAX
+
+#include "sgx_urts.h"
+#include "App.h"
+#include "Enclave_u.h"
+
+/* Global EID shared by multiple threads */
+sgx_enclave_id_t global_eid = 0;
+
+typedef struct _sgx_errlist_t {
+    sgx_status_t err;
+    const char *msg;
+    const char *sug; /* Suggestion */
+} sgx_errlist_t;
+
+/* Error code returned by sgx_create_enclave */
+static sgx_errlist_t sgx_errlist[] = {
+    {
+        SGX_ERROR_UNEXPECTED,
+        "Unexpected error occurred.",
+        NULL
+    },
+    {
+        SGX_ERROR_INVALID_PARAMETER,
+        "Invalid parameter.",
+        NULL
+    },
+    {
+        SGX_ERROR_OUT_OF_MEMORY,
+        "Out of memory.",
+        NULL
+    },
+    {
+        SGX_ERROR_ENCLAVE_LOST,
+        "Power transition occurred.",
+        "Please refer to the sample \"PowerTransition\" for details."
+    },
+    {
+        SGX_ERROR_INVALID_ENCLAVE,
+        "Invalid enclave image.",
+        NULL
+    },
+    {
+        SGX_ERROR_INVALID_ENCLAVE_ID,
+        "Invalid enclave identification.",
+        NULL
+    },
+    {
+        SGX_ERROR_INVALID_SIGNATURE,
+        "Invalid enclave signature.",
+        NULL
+    },
+    {
+        SGX_ERROR_OUT_OF_EPC,
+        "Out of EPC memory.",
+        NULL
+    },
+    {
+        SGX_ERROR_NO_DEVICE,
+        "Invalid SGX device.",
+        "Please make sure SGX module is enabled in the BIOS, and install SGX driver afterwards."
+    },
+    {
+        SGX_ERROR_MEMORY_MAP_CONFLICT,
+        "Memory map conflicted.",
+        NULL
+    },
+    {
+        SGX_ERROR_INVALID_METADATA,
+        "Invalid enclave metadata.",
+        NULL
+    },
+    {
+        SGX_ERROR_DEVICE_BUSY,
+        "SGX device was busy.",
+        NULL
+    },
+    {
+        SGX_ERROR_INVALID_VERSION,
+        "Enclave version was invalid.",
+        NULL
+    },
+    {
+        SGX_ERROR_INVALID_ATTRIBUTE,
+        "Enclave was not authorized.",
+        NULL
+    },
+    {
+        SGX_ERROR_ENCLAVE_FILE_ACCESS,
+        "Can't open enclave file.",
+        NULL
+    },
+};
+
+/* Check error conditions for loading enclave */
+void print_error_message(sgx_status_t ret)
+{
+    size_t idx = 0;
+    size_t ttl = sizeof sgx_errlist/sizeof sgx_errlist[0];
+
+    for (idx = 0; idx < ttl; idx++) {
+        if(ret == sgx_errlist[idx].err) {
+            if(NULL != sgx_errlist[idx].sug)
+                printf("Info: %s\n", sgx_errlist[idx].sug);
+            printf("Error: %s\n", sgx_errlist[idx].msg);
+            break;
+        }
+    }
+    
+    if (idx == ttl)
+    	printf("Error code is 0x%X. Please refer to the \"Intel SGX SDK Developer Reference\" for more details.\n", ret);
+}
+
+/* Initialize the enclave:
+ *   Call sgx_create_enclave to initialize an enclave instance
+ */
+int initialize_enclave(char *enclave_name)
+{
+    sgx_launch_token_t token = {0};
+    sgx_status_t ret = SGX_ERROR_UNEXPECTED;
+    int updated = 0;
+    
+    /* Call sgx_create_enclave to initialize an enclave instance */
+    /* Debug Support: set 2nd parameter to 1 */
+    ret = sgx_create_enclave(enclave_name, SGX_DEBUG_FLAG, &token, &updated, &global_eid, NULL);
+    if (ret != SGX_SUCCESS) {
+        print_error_message(ret);
+        return -1;
+    }
+
+    return 0;
+}
+
+/* OCall functions */
+void ocall_print_string(const char *str)
+{
+    /* Proxy/Bridge will check the length and null-terminate 
+     * the input string to prevent buffer overflow. 
+     */
+    printf("%s", str);
+}
+
+#define MAX_SIGNED_ENCLAVE_NAME 32
+#define NUMBER_OF_SIGNED_ENCLAVES 20
+static char enclave_names[NUMBER_OF_SIGNED_ENCLAVES][MAX_SIGNED_ENCLAVE_NAME] = {
+"enclave.signed.so",
+"enclave.signed.2MB.so",
+"enclave.signed.3MB.so",
+"enclave.signed.4MB.so",
+"enclave.signed.6MB.so",
+"enclave.signed.8MB.so",
+"enclave.signed.12MB.so",
+"enclave.signed.16MB.so",
+"enclave.signed.24MB.so",
+"enclave.signed.32MB.so",
+"enclave.signed.48MB.so",
+"enclave.signed.64MB.so",
+"enclave.signed.96MB.so",
+"enclave.signed.128MB.so",
+"enclave.signed.196MB.so",
+"enclave.signed.256MB.so",
+"enclave.signed.384MB.so",
+"enclave.signed.512MB.so",
+"enclave.signed.786MB.so",
+"enclave.signed.1GB.so"
+};
+
+static size_t buffer_sizes[NUMBER_OF_SIGNED_ENCLAVES] = {
+    1048576,            //1MiB
+    2097152,            //2MiB
+    3145728,            //3MiB
+    4194304,            //4MiB
+    6291456,            //6MiB
+    8388608,            //8MiB
+    12582912,           //12MiB
+    16777216,           //16MiB
+    25165824,           //24MiB
+    33554432,           //32MiB
+    50331648,           //48MiB
+    67108864,           //64MiB
+    100663296,          //96MiB
+    134217728,          //128MiB
+    205520896,          //196MiB
+    268435456,          //256MiB
+    402653184,          //384MiB
+    536870912,          //512MiB
+    824180736,          //786MiB
+    1073741824          //1GiB
+};
+
+#define NUMBER_OF_ENTRIES 50
+#define BILLION  1000000000.0
+#define PROTECTED_FILENAME "protectedFile"
+/* Application entry */
+int main(int argc, char *argv[])
+{
+    (void)(argc);
+    (void)(argv);
+    
+    double enclave_results[NUMBER_OF_SIGNED_ENCLAVES][NUMBER_OF_ENTRIES];
+    struct timespec start, end;
+    sgx_status_t ret = SGX_ERROR_UNEXPECTED;
+    size_t result = 0;
+
+    for (size_t i = 0; i < NUMBER_OF_SIGNED_ENCLAVES; i++)
+    {
+
+        for (size_t j = 0; j < NUMBER_OF_ENTRIES; j++)
+        {
+            if(initialize_enclave(enclave_names[i]) < 0)    return -1;                  // Enclave initialisation for writting (not included in the measurement)
+            ecall_allocate(global_eid, buffer_sizes[i]);                                // Buffer allocation for writting (not included in the measurement)
+
+            clock_gettime(CLOCK_REALTIME, &start);                                      // Benchmark start
+            ret = ecall_write_to_disk(global_eid, &result, PROTECTED_FILENAME, buffer_sizes[i]);    // Writting to disk with the appropriate buffer size 
+            if (ret != SGX_SUCCESS || result == 0)          return -1;           
+            sgx_destroy_enclave(global_eid);                                            // Destroying the enclave after writting
+            if(initialize_enclave(enclave_names[i]) < 0)    return -1;                  // Enclave initialising for reading
+            ret = ecall_read_from_disk(global_eid, &result, PROTECTED_FILENAME, buffer_sizes[i]);   // Reading from disk with the appropriate buffer size
+            if (ret != SGX_SUCCESS || result == 0)          return -1;           
+            clock_gettime(CLOCK_REALTIME, &end);                                        // Benchmark end
+            double time_spent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / BILLION;
+            enclave_results[i][j] = time_spent;
+            sgx_destroy_enclave(global_eid);                                            // Destroying the enclave after reading (not included in the measurement)
+        }   
+    }
+
+    FILE *fp;
+    fp = fopen("benchmark_results", "w");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Couldnt open or create a file for the benchmark data!\n");
+    }
+
+    for (size_t i = 0; i < NUMBER_OF_SIGNED_ENCLAVES; i++)
+    {
+        fprintf(fp, "%s raw data:\n", enclave_names[i]);
+        double average = 0;
+        for (size_t j = 0; j < NUMBER_OF_ENTRIES; j++)
+        {
+            fprintf(fp, "%f,", enclave_results[i][j]);
+            average += enclave_results[i][j];
+        }
+        average = average/(double) NUMBER_OF_ENTRIES;
+        fprintf(fp, "\ntotal average is: %f\n", average);
+        printf("%s average execution time is : %f seconds\n", enclave_names[i], average);
+    }
+    
+    fclose(fp);
+
+    printf("Info: Enclave initialisation benchmark successfully returned.\n");
+
+    printf("Enter a character before exit ...\n");
+    getchar();
+    return 0;
+}
+
---
+++ Enclave/Enclave.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2011-2018 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#include <stdarg.h>
+#include <stdio.h>      /* vsnprintf */
+#include <sgx_tprotected_fs.h> /* protected open/read/write functions */
+
+#include "Enclave.h"
+#include "Enclave_t.h"  /* print_string */
+
+char *buffer;
+
+/* 
+ * printf: 
+ *   Invokes OCALL to display the enclave buffer to the terminal.
+ */
+void printf(const char *fmt, ...)
+{
+    char buf[BUFSIZ] = {'\0'};
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, BUFSIZ, fmt, ap);
+    va_end(ap);
+    ocall_print_string(buf);
+}
+
+void free_buffer()
+{
+    if(buffer != NULL) free(buffer);
+}
+
+void ecall_allocate(size_t size) 
+{
+    buffer = (char *) malloc(size * sizeof(char));
+    if(buffer == NULL) printf("Error allocating buffer memory!\n");
+}
+
+
+size_t ecall_write_to_disk(const char *fname, size_t size)
+{
+    SGX_FILE* sFile = sgx_fopen_auto_key(fname, "w");
+    if(sFile == NULL)
+    {   
+        printf("failed to open the file!\n");
+        return 0;
+    } 
+
+    size_t result = sgx_fwrite(buffer, sizeof(char), size, sFile);
+    if(result != size)
+    {
+        printf("writting failed!\n");
+        return 0;
+    }
+
+    if(sgx_fclose(sFile) != 0)
+    {
+        printf("failed to close the file!\n");
+        return 0;
+    } 
+    free_buffer();
+    return result;
+}
+
+size_t ecall_read_from_disk(const char *fname, size_t size)
+{
+    SGX_FILE* sFile = sgx_fopen_auto_key(fname, "w");
+    if(sFile == NULL)
+    {   
+        printf("failed to open the file!\n");
+        return 0;
+    }
+    ecall_allocate(size);
+    size_t result = sgx_fread(buffer, sizeof(char), size, sFile);
+    if(result != size)
+    {
+        printf("reading failed!\n");
+        return 0;
+    }
+
+    if(sgx_fclose(sFile) != 0)
+    {
+        printf("failed to close the file!\n");
+        return 0;
+    } 
+    free_buffer();
+    return result;
+}
